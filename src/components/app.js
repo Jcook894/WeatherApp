e44487bc6964dd8060c614107a2501671ea64147
Jcook894 <jcook894@icloud.com>
2017-02-16 16:57:25 -0500

    Feat: Created search-bar comp. with search input and submit btn, then rendered that comp in the app class.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/search-bar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
-    );
+      <SearchBar />
+      );
   }
 }
