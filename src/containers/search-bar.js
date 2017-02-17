96f386bcd57193129cc49b020b3c05f252990ae6
Jcook894 <jcook894@icloud.com>
2017-02-16 20:15:32 -0500

    Feat: Added a on change handler and connected state. Also bound onInputChange to the search bar.

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event){
+    console.log(event.target.value);
+    this.setState({
+      term: event.target.value
+    });
+  }
+
   render(){
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Get a Five-day forecast in your favorite cities!"
+          value={this.state.term}
+          className="form-control"
+          onChange={this.onInputChange}
+          />
         <span className="input-group-btn">
 
           <button type="submit" className="btn btn-secondary">Submit</button>
