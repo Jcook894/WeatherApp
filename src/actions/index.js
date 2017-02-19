1bedb8ca2293bdff155d7c4a1ea1d9fdf7867e13
Jcook894 <jcook894@icloud.com>
2017-02-19 17:38:16 -0500

    Feat: Added root url for the API request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 const API_KEY =' b50d1586a5666074743eba40e46da683';
+const ROOT_URL = "http://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid=b1b15e88fa797225412429c1c50c122a1";
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
