03da0b84262a3d76bb848fa421f6c06a297a2965
Jcook894 <jcook894@icloud.com>
2017-02-19 18:34:52 -0500

    Feat: Made a AJAX request in the fetch weather action w/ axios. Connected that request to the payload.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
+import axios from 'axios';
+
 const API_KEY =' b50d1586a5666074743eba40e46da683';
-const ROOT_URL = "http://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid=b1b15e88fa797225412429c1c50c122a1";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid${API_KEY}`
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
