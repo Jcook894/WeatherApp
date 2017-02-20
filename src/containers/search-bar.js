54fc76b299f6628ab9e78679f2c351fe52362cfe
Jcook894 <jcook894@icloud.com>
2017-02-19 21:19:42 -0500

    Fix: Need to fix AJAX call in the action handler.

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
 
     this.state = {
       term: ''
     };
+
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event){
@@ -19,6 +24,10 @@ export default class SearchBar extends Component {
 
 onFormSubmit(event) {
   event.preventDefault();
+
+  //Need to go fetch weather data.
+  this.props.fetchWeather(this.state.term);
+  this.setState({term: ''});
 }
 
   render(){
@@ -37,5 +46,11 @@ onFormSubmit(event) {
           </span>
       </form>
     )
-  }
+  };
 }
+
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather}, dispatch);
+}
+ export default connect(null, mapDispatchToProps)(SearchBar);
