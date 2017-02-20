637d1017262b0c083d83210f206027ccc6ad6e87
Jcook894 <jcook894@icloud.com>
2017-02-20 01:29:18 -0500

    Feat: Set up list for cities and mapped the state to the weatherlist props.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
   render() {
     return(
       <table className="table table-hover">
         <thead>
           <tr>
-            <th>City:</th>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
 
         </thead>
           <tbody>
-          
+
           </tbody>
       </table>
     )
   }
 }
+
+
+ function mapStateToProps({weather}){
+   return {weather};
+
+ }
+
+
+export default connect(mapStateToProps)(WeatherList);
