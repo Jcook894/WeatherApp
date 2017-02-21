4aa219b789b84cd21045b6694520fce309532112
Jcook894 <jcook894@icloud.com>
2017-02-21 03:08:36 -0500

    Feat: Added the data for pressure and humidity and added that data to the chart comps.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,14 +7,16 @@ class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name;
     const temp = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
 
     console.log(temp);
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart data={temp} color="red" />
-        </td>
+        <td><Chart data={temp} color="red" /></td>
+        <td><Chart data={pressures} color="green" /></td>
+        <td><Chart data={humidity} color="blue" /></td>
       </tr>
     );
 
